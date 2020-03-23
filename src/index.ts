80d66f81fe0b60b6adf5a4d43cb873dbf968b5d2
g-six <gerard@idearobin.com>
2020-03-23 07:11:10 +0000

    feat: 🎸 generatePassword

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import * as database from './database'
 
 let lang
 
-export const loadLocale = file => {
+export const loadLocale = (file) => {
   try {
     lang = safeLoad(readFileSync(file, 'utf8'))
   } catch (e) {
@@ -30,16 +30,30 @@ export const invalidRequestReducer = (errors: ValidationErrorItem[]) => {
   }
 }
 
-export const kebabCase = input => {
+export const kebabCase = (input) => {
   return input.split(' ').join('-')
 }
 
-export const generateUri = input => {
+export const generateUri = (input) => {
   const lcase = input
     .toLowerCase()
     .replace(/[^\w ]+/g, '')
     .replace(/ +/g, '-')
   return kebabCase(lcase)
+}
+
+export const symbols = `<>@!#$%^&*()-+{};:?/|][`
+export const generatePassword = (length = 8, add_symbols = false) => {
+  const charset = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789${
+    add_symbols ? symbols : ''
+  }`
+  let ret_val = ''
+
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    ret_val += charset.charAt(Math.floor(Math.random() * n))
+  }
+
+  return ret_val
 }
 
 export const Database = database
