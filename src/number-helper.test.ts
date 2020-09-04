e7f45f2cfa0d3faa0d888c646688d27cd2cd2db1
John Gerald Agbayani <john.agbayani@idearobin.com>
2020-09-04 06:02:25 +0000

    feat: 🎸 Allow humanization of negative numbers

--- src/number-helper.test.ts
+++ src/number-helper.test.ts
@@ -1,6 +1,6 @@
 import { humanize } from './number-helper'
 
-describe('humanize', () => {
+describe('humanize positive numbers', () => {
   it('should humanize by millions', () => {
     const actual = humanize(1555000)
 
@@ -17,5 +17,25 @@ describe('humanize', () => {
     const actual = humanize(555)
 
     expect(actual).toBe(555)
+  })
+})
+
+describe('humanize negative numbers', () => {
+  it('should humanize by millions', () => {
+    const actual = humanize(-1555000)
+
+    expect(actual).toBe('-1.55M')
+  })
+
+  it('should humanize by thousands', () => {
+    const actual = humanize(-1555)
+
+    expect(actual).toBe('-1.55K')
+  })
+
+  it('should humanize', () => {
+    const actual = humanize(-555)
+
+    expect(actual).toBe('-555')
   })
 })
